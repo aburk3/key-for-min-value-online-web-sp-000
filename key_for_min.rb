40c5be8917f1b3dece2277636371534d809e3e20
Austin  Burke <aburk3@gmail.com>
2018-10-03 10:25:41 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@ require 'pry'
 
 def key_for_min_value(name_hash)
     lowest_value = nil
-    lowest_value_name = nil
+    lowest_key_name = nil
     name_hash.collect do |name, value|
       if lowest_value == nil || lowest_value > value 
         lowest_value = value
